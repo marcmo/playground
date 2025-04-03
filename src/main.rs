397ef7944c8bc4d8334d25b7698165a5bafafad7
sudeep.tarlekar <sudeep.tarlekar@accenture.com>
2025-04-03 11:26:26 +0200

    Added repo change
    Change-Id: I7a09bf5fd74a9e31308713bd0590bf6bf5018de6

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use anyhow::Context;
+use anyhow::Result;
+
 fn main() {
     println!("Hello, world!");
+}
+
+fn fetch_repo() -> Result<git2::Repository> {
+    git2::Repository::open(".").context("Current working directory is not a git repo")
 }
