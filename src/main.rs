7e9086ca7662a08c0db57bea898fabd77c53e320
sudeep.tarlekar <sudeep.tarlekar@accenture.com>
2025-04-24 12:29:06 +0200

    Add checksum details
    Change-Id: Ief70cb9b0362275a29280f6a73e92a32f8402134

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,11 @@ fn main() {
     println!("Lorem Ipsum.....");
     println!("Rim info file should start with {}", dictionary::RIM_INFO);
     print_rim_details();
+    println!(
+        "If url starts with {} then we are talking to Gerrit server",
+        dictionary::GERRIT_SERVER
+    );
+    println!("{} is important in rim calculation", dictionary::CHECKSUM);
 }
 
 fn print_rim_details() {
