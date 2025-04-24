3738300878b099ce487c8a43f1fe6c465ffe64a3
sudeep.tarlekar <sudeep.tarlekar@accenture.com>
2025-04-24 12:27:18 +0200

    Add riminfo details
    Change-Id: I711ee60b56544c7355b63bdfb2f6be25426efefe

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
+use dictionary;
+
 fn main() {
     println!("Hello, world!");
     println!("Lorem Ipsum.....");
     println!("Rim info file should start with {}", dictionary::RIM_INFO);
+    print_rim_details();
+}
+
+fn print_rim_details() {
+    println!("Riminfo file name {}", dictionary::RIM_INFO);
 }
