649d4e1cc9f1d46fe2ec24589204847f94aeabe8
sudeep.tarlekar <sudeep.tarlekar@accenture.com>
2025-05-07 15:34:49 +0200

    Add function for reading .riminfo from dir
    Change-Id: Ie1c31581a3f2a6a6af4716fac19dd2fee014877d

--- src/main.rs
+++ src/main.rs
@@ -3,3 +3,11 @@ fn main() {
     println!("Lorem Ipsum.....");
     println!("Rim info file should start with {}", dictionary::RIM_INFO);
 }
+
+fn function_for_force_push() {
+    println!("Created random function for force pushing");
+}
+
+fn random_func() {
+    println!("Some random function");
+}
