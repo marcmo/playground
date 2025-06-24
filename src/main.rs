f8a2f317ab07aa5f58a3b4a96bb42655ce737084
sudeep.tarlekar <sudeep.tarlekar@accenture.com>
2025-06-24 16:08:15 +0200

    First change in relation chain
    Change-Id: I1379baf5eba688591bca7c15cfb6b6aa4a71525b

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,6 @@ fn main() {
     println!("Hello, world!");
     println!("Lorem Ipsum.....");
     println!("Rim info file should start with {}", dictionary::RIM_INFO);
+
+    println!("Change to test the relation chain");
 }
