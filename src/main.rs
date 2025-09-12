1805bf52fd5ba726e91f1ee3380385af79fb5b58
test.user <test.user@accenture.com>
2025-09-12 12:10:38 +0530

    Testing PR
    Change-Id: I91fd5c4f34c2a64c392796b3a6ad60e40cc07f1f

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     println!("Hello, world!");
     println!("Lorem Ipsum.....");
+    println!("Testing.....");
     println!("Rim info file should start with {}", dictionary::RIM_INFO);
     println!("Rim info file should start with {}", dictionary::RIM_INFO);
     println!("Rim info file should start with {}", dictionary::RIM_INFO);
