f146bb87949b16bd29b4612e0319bf3206f9a912
Shamitha Shashidhara <shamitha.shashidhara@accenture.com>
2025-11-13 11:39:41 +0530

    relational chain2
    Change-Id: I3cc54a28842dd16e4654192133a2427986f1b059

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     println!("Hello, world!");
     println!("Lorem Ipsum.....");
+    println!("Lorem Ipsum.....");
     println!("Rim info file should start with {}", dictionary::RIM_INFO);
     println!("Rim info file should start with {}", dictionary::RIM_INFO);
     println!("Rim info file should start with {}", dictionary::RIM_INFO);
