353f4db148baad6f0901c0af83931448a4f15acd
SuhashiniNaik <suhashini.naik@accenture.com>
2025-11-14 08:37:13 +0000

    Test commit 1
    Change-Id: Ic784a5fe6523c6dd7e790d688601735566703e8f

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use git2::{Remote, Repository};
 
 fn main() {
     println!("Hello, world!");
+    
     println!("Lorem Ipsum.....");
     println!("Rim info file should start with {}", dictionary::RIM_INFO);
     println!("Rim info file should start with {}", dictionary::RIM_INFO);
