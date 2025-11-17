095504484c54f0a67879bb9a839eb16cf21904c1
SuhashiniNaik <suhashini.naik@accenture.com>
2025-11-17 10:52:25 +0000

    Delete change-Id from commit

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use git2::{Remote, Repository};
 fn main() {
     println!("Hello, world!");
     println!("Lorem Ipsum.....");
+    println!("This commit on Github should not print the change ID");
     println!("Rim info file should start with {}", dictionary::RIM_INFO);
     println!("Rim info file should start with {}", dictionary::RIM_INFO);
     println!("Rim info file should start with {}", dictionary::RIM_INFO);
